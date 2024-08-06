2a770c9b6116c43b75d4985009ef80abf778f122
jhegeman <jeroen.hegeman@cern.ch>
2024-08-06 11:44:02 +0200

    Add one more HDL code as failures for check_syntax (since this will make synth fail anyway)

--- src/ipbb/cmds/vivado.py
+++ src/ipbb/cmds/vivado.py
@@ -203,6 +203,7 @@ def checksyntax(ictx):
         'HDL 9-1752',
         'HDL 9-1797',
         'HDL 9-3136',
+        'HDL 9-3314',
         'HDL 9-3500',
         'HDL 9-3609',
         'HDL 9-3907',
